6db621712457711fc865521966de1a56aaccfe9e
homework2 <amosovaoksana16@gmail.com>
2022-08-25 23:32:44 +0500

    Мой первый коммит 1 коммит 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
+        byte brush = 126;
+        System.out.println("Значение переменной кисть с типом byte равно " + brush);
+        short matches = 32750;
+        System.out.println("Значение переменной спички с типом short равно" + matches);
+        int cactus = 999;
+        System.out.println("Значение переменной кактус с типом int равно" + cactus);
+        long largePits = 898476300L;
+        System.out.println("Значение переменной большие ямы с типом long равно"+ largePits);
+        float colors = 3.363637f;
+        System.out.println("Значение переменной цветов с типом float" + colors);
+        double rackets = 2.47473839930930;
+        System.out.println("Значение переменной ракеток с типом double" + rackets);
+
+        //Задача 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
